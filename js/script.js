49c11870cbaa1816ffcc02ba7ed428eeb8739e5c
sebknight <37192621+sebknight@users.noreply.github.com>
2018-08-13 22:09:12 +1200

    tweaks

--- js/script.js
+++ js/script.js
@@ -16,11 +16,9 @@ $(document).ready(function(){
     })
 
     function getDog() {
-        $("#loader").show();
         $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
             $("#picture").html(JSON.stringify(data, null, 4));
             $("#picture").html("<img src='" + data.message + "'>");
-            $("#loader").hide();
         });
     }
 
