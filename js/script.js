a988339cbc21011e3b41f5bc30edbc016def6198
sebknight <37192621+sebknight@users.noreply.github.com>
2018-08-13 21:48:04 +1200

    font changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    var contactDropdown = $('.dropdown__contact');
+    var aboutDropdown = $('.dropdown__about');
+
+    function showHide(buttonDropdown, opposingDropdown) {
+        $(buttonDropdown).toggle();
+        $(opposingDropdown).hide();
+    }
+
+    $('#about').click(function () {
+        showHide(aboutDropdown, contactDropdown);
+    })
+
+    $('#contact').click(function () {
+        showHide(contactDropdown, aboutDropdown);
+    })
+
     function getDog() {
         $("#loader").show();
         $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
@@ -10,18 +26,6 @@ $(document).ready(function(){
 
     $('#fetch').click(function () {
         getDog();
-    });
-
-    $('#about').click(function(){
-        // $('.dropdown').not(this).hide();
-        $('.dropdown__about').toggle();
     })
 
-    $('#contact').click(function () {
-        // $('.dropdown').not(this).hide();
-        $('.dropdown__contact').toggle();
-
-
-
-    })
 })
