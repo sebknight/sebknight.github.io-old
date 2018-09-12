6f41a545511e3402e0cc7f8ebbfca94b529050fd
sebknight <37192621+sebknight@users.noreply.github.com>
2018-09-13 08:05:59 +1200

    added mockups

--- js/script.js
+++ js/script.js
@@ -15,6 +15,41 @@ $(document).ready(function(){
         showHide(contactDropdown, aboutDropdown);
     })
 
+    $('.gallery').slick({
+        speed: 300,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        centerMode: true,
+        adaptiveHeight: true
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+
     function getDog() {
         $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
             $("#picture").html(JSON.stringify(data, null, 4));
